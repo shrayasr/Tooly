aa109dc82bfd73738777945d35bf21a89505d3ad
Shrayas Rajagopal <shrayasr@logicsoft.co.in>
2022-07-10 22:44:19 +0530

    Add PG table generator

---
+++ Tooly/Models/Key.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tooly.Models
+{
+    public class Key
+    {
+        public string TableName { get; private set; }
+        public string KeyType { get; set; }
+        public string PKColName { get; set; }
+        public string FKFromColName { get; set; }
+        public string FKToTableName { get; set; }
+        public string FKToColName { get; set; }
+        public string UKColName { get; set; }
+
+        public Key(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public override string ToString()
+        {
+            var constraintName = "";
+            var constraintDetails = "";
+
+            if (KeyType.ToUpper() == "PK")
+            {
+                constraintName = $"{TableName.ToLower()}_pkey";
+                constraintDetails = $"PRIMARY KEY ({PKColName})";
+            }
+            else if (KeyType.ToUpper() == "FK")
+            {
+                constraintName = $"{TableName.ToLower()}_{FKToTableName.ToLower().Replace(".", "_")}_fk";
+                constraintDetails = $"FOREIGN KEY ({FKFromColName}) REFERENCES {FKToTableName}({FKToColName})";
+            }
+            else if (KeyType.ToUpper() == "UK")
+            {
+                constraintName = $"{TableName.ToLower()}_ukey";
+                constraintDetails = $"UNIQUE ({UKColName})";
+            }
+
+            return $"CONSTRAINT {constraintName} {constraintDetails}";
+        }
+    }
+}
---
+++ Tooly/Util/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tooly.Util
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string instance)
+            => instance[0].ToString().ToUpper() + instance.Substring(1);
+
+        public static bool IsEmpty(this string instance)
+            => string.IsNullOrEmpty(instance);
+
+    }
+}
